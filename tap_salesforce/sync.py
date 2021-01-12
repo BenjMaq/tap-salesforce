e4bbc0bf00f1d9af8465cccb4efe48b0463d236f
Benjamin Maquet <bmaquet@twilio.com>
2021-01-12 10:12:29 +0100

    Add print statement

--- tap_salesforce/sync.py
+++ tap_salesforce/sync.py
@@ -118,6 +118,8 @@ def sync_records(sf, catalog_entry, state, counter):
     stream_alias = catalog_entry.get('stream_alias')
     catalog_metadata = metadata.to_map(catalog_entry['metadata'])
     replication_key = catalog_metadata.get((), {}).get('replication-key')
+    LOGGER.info('replication-key for %s is %s', catalog_entry, replication_key)
+
     stream_version = get_stream_version(catalog_entry, state)
     activate_version_message = singer.ActivateVersionMessage(stream=(stream_alias or stream),
                                                              version=stream_version)
